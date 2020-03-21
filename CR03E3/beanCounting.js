23814e753bc48669e574d299dd3040b2d6403436
Makeshiftdev-c <abde_321@hotmail.com>
2020-03-21 02:13:38 +0000

    Chapter 3, Exercise 3

---
+++ CR03E3/beanCounting.js
@@ -0,0 +1,14 @@
+function countChar(str, ch)
+{
+	let count = 0;
+
+	for (let i = 0; i < str.length; i++)
+		if (str[i] === ch)
+			count++;
+	return count;
+}
+
+function countBs(str)
+{
+	return countChar(str, "B");
+}
